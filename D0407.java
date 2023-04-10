dc33027c13763fbe312bd1ebb3bf8765710ac02b
김슬기 <130130090+KSeGi@users.noreply.github.com>
2023-04-10 09:00:28 +0900

    Add files via upload
    0407숙제

---
+++ D0407.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package d0407;
+
+/**
+ * @author student
+ *
+ */
+public class D0407 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
